01cebf8c2f03f605482fb17408aef4c23f88381b
Nga Nguyen <nguyentuyetnga0209@gmail.com>
2023-11-24 14:43:27 +0700

    finish class Student

--- OPP/Program.cs
+++ OPP/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using OPP;
+
+//object by constructor
+//Student stu1 = new Student(1, "nguyen", true, new DateTime(1982,04,27));
+//Student stu2 = new(2, "nguyen", true, new DateTime(1982,04,27));
+
+//object initializer => class không cần tạo constructor làm gì
+Student stu3 = new()
+{
+    Id = 3,
+    Gender = false,
+    Fullname = "trong",
+    Dob = DateTime.Now
+};
+
+Console.WriteLine(stu3);
--- OPP/Student.cs
+++ OPP/Student.cs
@@ -1,10 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OPP;
+﻿namespace OPP;
 internal class Student
 {
+    //vừa là field vừa là auto property
+    public int Id { get; set; }
+    public string? Fullname { get; set; }
+    public bool Gender { get; set; }
+    public DateTime Dob { get; set; }
+
+    //constructor
+    //public Student() { }
+
+    //public Student(int id, string? fullname, bool gender, DateTime dob)
+    //{
+    //    Id = id;
+    //    Fullname = fullname;
+    //    Gender = gender;
+    //    Dob = dob;
+    //}
+
+    //override tostring 
+    public override string ToString()
+    {
+        return $"{nameof(Id)}={Id.ToString()}, {nameof(Fullname)}={Fullname}, {nameof(Gender)}={Gender.ToString()}, {nameof(Dob)}={Dob.ToString()}";
+    }
 }
